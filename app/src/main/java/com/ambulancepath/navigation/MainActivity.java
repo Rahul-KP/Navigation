4c03427708ee3c3dd321c17e95bd76d754680cac
Amitash M S <amitu895@gmail.com>
2022-10-14 17:40:00 +0530

    Added options menu

--- app/src/main/java/com/ambulancepath/navigation/MainActivity.java
+++ app/src/main/java/com/ambulancepath/navigation/MainActivity.java
@@ -1,9 +1,13 @@
 package com.ambulancepath.navigation;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,11 +25,36 @@ public class MainActivity extends AppCompatActivity {
         checkPermissions();
     }
 
+    // Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.quit:
+                System.exit(0);
+                return true;
+            case R.id.skip:
+                Toast.makeText(this, "Loading Map", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(this, MapActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            startActivity(new Intent(this, MapActivity.class));
+//            startActivity(new Intent(this, MapActivity.class));
         } else {
             Toast.makeText(this, "Permission not granted!", Toast.LENGTH_SHORT).show();
         }
@@ -33,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void checkPermissions() {
         if(checkSelfPermission(PERMISSIONS_REQUIRED[0]) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(PERMISSIONS_REQUIRED[1]) == PackageManager.PERMISSION_GRANTED) {
-            startActivity(new Intent(this, MapActivity.class));
+//            startActivity(new Intent(this, MapActivity.class));
         } else {
             requestPermissions(PERMISSIONS_REQUIRED, 100);
         }
